206731d264e3e0e525a1b1968fc3d2b60b8a3169
Maicol Jimenez <maicol_jimenez82121@elpoli.edu.co>
2022-09-14 22:29:36 -0500

    updating  path

--- Routes/disciplines/Discipline.js
+++ Routes/disciplines/Discipline.js
@@ -2,7 +2,7 @@ const  expres = require('express')
 const  app = expres();
 require('module-alias/register')
 const { check } = require('express-validator')
-const  Disciplina = require('@models/discipline/discipline');
+// const  Disciplina = require('@models/discipline/discipline');
 const { getItems, createItem, updateItem } = require('@Controllers/discipline/Discipline');
 
 const { ExistById } = require('@helpers/Validators/dbValidators');
@@ -39,39 +39,39 @@ app.post('/', [
  * deleted (todo) discipline  registered 
  */
 
-app.delete('/:id', token.verificatoken, (req, res) => {
+// app.delete('/:id', token.verificatoken, (req, res) => {
 
-    const  id = req.params.id;
-    let  data ;
+//     const  id = req.params.id;
+//     let  data ;
 
-    Disciplina.deleteOne({_id: id}, (err, disciplinaborrado) => {
-
-
-        if (err) {
-            return res.status(500).json({
-                ok: false,
-                mensaje: 'Error borar disciplina',
-                erros: err
-            });
-        }
-        if (!disciplinaborrado) {
-            return res.status(400).json({
-                ok: false,
-                mensaje: 'persona con ' + id + ' no existe',
-                erros: { message: 'no existe el persona con ese id ' }
-            });
-        }
-        data = disciplinaborrado;
-        res.status(200).json({
-            ok: true,
-            usuario: data
-        });
+//     Disciplina.deleteOne({_id: id}, (err, disciplinaborrado) => {
 
 
-    });
+//         if (err) {
+//             return res.status(500).json({
+//                 ok: false,
+//                 mensaje: 'Error borar disciplina',
+//                 erros: err
+//             });
+//         }
+//         if (!disciplinaborrado) {
+//             return res.status(400).json({
+//                 ok: false,
+//                 mensaje: 'persona con ' + id + ' no existe',
+//                 erros: { message: 'no existe el persona con ese id ' }
+//             });
+//         }
+//         data = disciplinaborrado;
+//         res.status(200).json({
+//             ok: true,
+//             usuario: data
+//         });
 
 
-});
+//     });
+
+
+// });
 
 
 
